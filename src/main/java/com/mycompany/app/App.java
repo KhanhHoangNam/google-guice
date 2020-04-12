bfaaba84eb55d13eef59dd7b6f058c96b8e315b5
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2020-04-12 11:48:43 +0700

    First Commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,7 +3,6 @@ package com.mycompany.app;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Hello world!
@@ -12,17 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(Calendar.SATURDAY);
-
-//        Calendar cal = Calendar.getInstance();
-//        Date date = cal.getTime();
-//
-//        System.out.println(cal);
-//
-//        if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
-//            System.out.println("Sunday!");
-//        }
-//
-//        System.out.println(date);
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'17:00:00.000");
         Calendar cal = Calendar.getInstance();
